da01c6114970ee71ef5301f07510e5aca84cd9cb
calvincurry22 <calvincurry22@gmail.com>
2020-08-20 12:01:19 -0500

    Added test project to FoodLocker solution

---
+++ FoodLocker.Tests/EFTestFixture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FoodLocker.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace FoodLocker.Tests
+{
+    class EFTestFixture
+    {
+    }
+}
