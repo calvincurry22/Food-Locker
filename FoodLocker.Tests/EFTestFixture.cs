60656870c117c9bafd04d5c1f4372d6795e2fd77
calvincurry22 <calvincurry22@gmail.com>
2020-08-20 20:00:24 -0500

    Added code to open and close the Sqlite connection in EFTextFixture.cs

--- FoodLocker.Tests/EFTestFixture.cs
+++ FoodLocker.Tests/EFTestFixture.cs
@@ -8,7 +8,39 @@ using System.Linq;
 
 namespace FoodLocker.Tests
 {
-    class EFTestFixture
+    public class TestDbContext: ApplicationDbContext
     {
+        public TestDbContext(DbContextOptions<ApplicationDbContext> options) : base (options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Model.GetEntityTypes()
+                .Where(e => !e.IsOwned())
+                .SelectMany(e => e.GetForeignKeys())
+                .ToList()
+                .ForEach(relationship => relationship.DeleteBehavior = DeleteBehavior.Restrict);
+        }
+    }
+    public abstract class EFTestFixture : IDisposable
+    {
+        private const string _connectionString = "DataSource=:memory:";
+        private readonly SqliteConnection _connection;
+
+        protected readonly TestDbContext _context;
+
+        protected EFTestFixture()
+        {
+            _connection = new SqliteConnection(_connectionString);
+            _connection.Open();
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlite(_connection)
+                    .Options;
+            _context = new TestDbContext(options);
+            _context.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
     }
 }
